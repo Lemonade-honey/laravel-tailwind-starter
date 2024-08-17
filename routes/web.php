df2dc20e0ed83efa3669617e34edc8251d2dd364
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-08-17 19:24:22 +0700

    feat: alur awal strukutr path

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::middleware('guest')->group(function(){
+    Route::get('/login')->name('login');
+    Route::post('/login');
+    Route::get('/register')->name('register');
+    Route::post('/register');
+});
+
+// auth path
+Route::middleware(['auth'])->group(function(){
+    Route::get('logout')->name('logout');
+
+    Route::prefix('dashboard')->group(function(){
+        Route::get('/')->name('dashboard');
+    });
+});
