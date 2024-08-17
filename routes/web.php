81058a7d1f460428f37949ff734bf29aca5a33e4
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-08-17 21:51:42 +0700

    feat: dasahboard done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,6 +20,6 @@ Route::middleware(['auth'])->group(function(){
     Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
     Route::prefix('dashboard')->group(function(){
-        Route::get('/')->name('dashboard');
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     });
 });
